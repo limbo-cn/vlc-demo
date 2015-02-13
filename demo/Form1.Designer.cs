fa1dfee30a0a1e5673a65ecb53c81fad5f07e880
limbo-cn <317091439@qq.com>
2015-02-13 09:13:47 +0800

    add pro
    commit to github

---
+++ demo/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace vlc.net
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tbVideoTime = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(602, 360);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.tbVideoTime);
+            this.panel2.Controls.Add(this.trackBar2);
+            this.panel2.Controls.Add(this.trackBar1);
+            this.panel2.Controls.Add(this.btnReset);
+            this.panel2.Controls.Add(this.btnStart);
+            this.panel2.Location = new System.Drawing.Point(0, 361);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(602, 67);
+            this.panel2.TabIndex = 1;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar2.AutoSize = false;
+            this.trackBar2.Location = new System.Drawing.Point(432, 26);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(170, 28);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Location = new System.Drawing.Point(0, 0);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(493, 25);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(89, 27);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 34);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "关闭";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(8, 27);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 34);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tbVideoTime
+            // 
+            this.tbVideoTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbVideoTime.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbVideoTime.Location = new System.Drawing.Point(492, 3);
+            this.tbVideoTime.Name = "tbVideoTime";
+            this.tbVideoTime.ReadOnly = true;
+            this.tbVideoTime.Size = new System.Drawing.Size(108, 14);
+            this.tbVideoTime.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 428);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "libvlc.net播放器";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox tbVideoTime;
+    }
+}
+
---
+++ demo/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace vlc.net
+{
+    public partial class Form1 : Form
+    {
+        private VlcPlayer vlc_player_;
+        private bool is_playinig_;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string pluginPath = System.Environment.CurrentDirectory + "\\plugins";
+            vlc_player_ = new VlcPlayer(pluginPath);
+            IntPtr render_wnd = this.panel1.Handle;
+            vlc_player_.SetRenderWindow((int)render_wnd);
+
+            tbVideoTime.Text = "00:00:00/00:00:00";
+
+            is_playinig_ = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                vlc_player_.PlayFile(ofd.FileName);
+                trackBar1.SetRange(0, (int)vlc_player_.Duration());
+                trackBar1.Value = 0;
+                timer1.Start();
+                is_playinig_ = true;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            if (is_playinig_)
+            {
+                vlc_player_.Stop();
+                trackBar1.Value = 0;
+                timer1.Stop();
+                is_playinig_ = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (is_playinig_)
+            {
+                if (trackBar1.Value == trackBar1.Maximum)
+                {
+                    vlc_player_.Stop();
+                    timer1.Stop();
+                }
+                else
+                {
+                    trackBar1.Value = trackBar1.Value + 1;
+                    tbVideoTime.Text = string.Format("{0}/{1}", 
+                        GetTimeString(trackBar1.Value), 
+                        GetTimeString(trackBar1.Maximum));
+                }
+            }
+        }
+
+        private string GetTimeString(int val)
+        {
+            int hour = val / 3600;
+            val %= 3600;
+            int minute = val / 60;
+            int second = val % 60;
+            return string.Format("{0:00}:{1:00}:{2:00}", hour, minute, second);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (is_playinig_)
+            {
+                vlc_player_.SetPlayTime(trackBar1.Value);
+                trackBar1.Value = (int)vlc_player_.GetPlayTime();
+            }
+        }
+    }
+}
